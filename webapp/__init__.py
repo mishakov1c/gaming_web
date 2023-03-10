5354314849af1384a3fd5f8ee559f7a3553a92b6
Konstantin Mishakov <mishakov1c@gmail.com>
2023-03-11 00:18:10 +0700

    Добавлен get_post()

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 from webapp.article_model import db, Articles
 
+def get_post(post_id):
+    post = Articles.query.get_or_404(post_id)
+    return post
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +17,11 @@ def create_app():
         news_list = Articles.query.order_by(Articles.written.desc()).all()
         return render_template('index.html', page_title = title, news_list = news_list)
     
+    @app.route('/<int:post_id>')
+    def post(post_id):
+        post = get_post(post_id)
+        return render_template('post.html', page_title = post.title, post_text=post.text) 
+
     with app.app_context():
         db.create_all()
 
