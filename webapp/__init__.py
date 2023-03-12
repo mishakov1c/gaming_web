00b13f29e29193835d1e667990fed186e408bc12
Konstantin Mishakov <mishakov1c@gmail.com>
2023-03-12 11:20:01 +0700

    Добавлен черновик страницы поста и редактирования поста

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 from webapp.article_model import db, Articles
+from webapp.dtf_news import get_dtf_news
 
 def get_post(post_id):
     post = Articles.query.get_or_404(post_id)
     return post
+
 
 def create_app():
     app = Flask(__name__)
@@ -15,7 +17,15 @@ def create_app():
     def index():
         title = 'Новости игр'
         news_list = Articles.query.order_by(Articles.written.desc()).all()
+        # news_list = get_dtf_news()
         return render_template('index.html', page_title = title, news_list = news_list)
+    
+    
+    @app.route('/edit/<int:post_id>')
+    def edit_post(post_id):
+        post = get_post(post_id)
+        return render_template('edit_post.html', page_title = post.title, post_text=post.text, post_id=post_id)
+    
     
     @app.route('/<int:post_id>')
     def post(post_id):
@@ -26,3 +36,10 @@ def create_app():
         db.create_all()
 
     return app
+ 
+
+    
+
+    
+
+    
