8338fb7e1a76e79bd7f58e3f67bf1438fc10ca2e
Pavel Mager <farmarker@gmail.com>
2023-03-19 16:52:16 +0100

    register system

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, url_for
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_migrate import Migrate
 from webapp.config import MAIN_PAGE
-from webapp.forms import LoginForm, ArticleForm
+from webapp.forms import LoginForm, ArticleForm, RegisterForm
 from webapp.model import db, Articles, User
 
 
@@ -11,7 +11,7 @@ def get_post(post_id):
     post = Articles.query.get_or_404(post_id)
     return post
 
-
+# Создание web приложения
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
@@ -26,6 +26,7 @@ def create_app():
     def load_user(user_id):
         return User.query.get(user_id)
 
+# Переход на главную страницу
     @app.route('/')
     def index():
         news_list = Articles.query.order_by(Articles.written.desc()).all()
@@ -103,9 +104,53 @@ def create_app():
 
         return render_template('edit_post.html', post=post, written = written_string, edited = edited)
     
+    @app.route('/register')
+    def register():
+        r"""Проверяем, если пользователь залогинен.
+            Если ДА, перенаправляем его на страницу index.html
+            Если НЕТ, перенаправляем его на страницк register.html"""
 
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        title = 'Регистрация'
+        register_form = RegisterForm()
+        return render_template('register.html', page_title=title, form=register_form)
+    @app.route('/process-register', methods=['GET', 'POST'])
+    def process_register():
+
+        form = RegisterForm()
+
+        if form.validate_on_submit():
+            username = form.username
+            email = form.email
+            password1 = form.password.data
+            password2 = form.confirm_password.data
+
+            if User.query.filter_by(username=username.data).first():
+                flash('Пользователь с таким именем уже существует!')
+                print(User.username)
+                return redirect(url_for('register'))
+            elif User.query.filter_by(email=email.data).first():
+                flash('Пользователь с таким имейлом уже существует!')
+                return redirect(url_for('register'))
+            elif not password1 == password2:
+                flash('Пароли не одинаковые!')
+                return redirect(url_for('register'))
+                
+            new_user = User(username=username.data, email=email.data, role='user')
+            new_user.set_password(password1)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            flash('Вы успешно зарегистрировались')
+            return redirect(url_for('index'))
+        
     @app.route('/login')
     def login():
+        r"""Проверяем, если пользователь залогинен.
+            Если ДА, перенаправляем его на страницу index.html
+            Если НЕТ, перенаправляем его на страницк login.html"""
+
         if current_user.is_authenticated:
             return redirect(url_for('index'))
         title = 'Авторизация'
@@ -114,6 +159,13 @@ def create_app():
 
     @app.route('/process-login', methods=['POST'])
     def process_login():
+        r"""Маршрут, который обрабатывает информацию, полученную из полей
+        данных для входа в аккаунт в login.html. Создается форма из flask-login,
+        Потом проверяем, была ли отправлена форма и прошла ли она валидацию.
+        Мы берем пользователя из баззы данных, чей юзернейм нам передали и 
+        проверяем схожесть паролей. Если всё ОК, то пользователь заходит в 
+        сеть под этим ником и остается онлайн с помощью flask-login"""
+
         form = LoginForm()
 
         if form.validate_on_submit():
@@ -140,8 +192,8 @@ def create_app():
         else:
             return 'Ты не Админ!'
 
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     #     db.drop_all()
 
     return app
