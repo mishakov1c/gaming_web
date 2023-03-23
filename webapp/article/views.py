ef8e06fbbb7a1d2c293a7d8a47c192d68ae05bfb
Konstantin Mishakov <mishakov1c@gmail.com>
2023-03-23 20:17:52 +0700

    index: added pic and descrion

--- webapp/article/views.py
+++ webapp/article/views.py
@@ -15,7 +15,6 @@ def get_post(post_id):
 @blueprint.route('/')
 def index():
     news_list = Articles.query.filter(Articles.is_published == True)
-    print(news_list)
     return render_template('news/index.html', news_list = news_list, current_user=current_user)
 
 
