9ab38a9bf1e8d55d56e980a12a55a67680cbe4e0
Pavel Mager <farmarker@gmail.com>
2023-03-24 17:47:06 +0100

    Merge branch 'main' of https://github.com/mishakov1c/gaming_web

--- webapp/forms.py
+++ webapp/forms.py
@@ -12,7 +12,7 @@ class ArticleForm(FlaskForm):
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={'class': "form-control"})
     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class': "form-control"})
-    remember_me = BooleanField('Запомнить меня', default=True, render_kw={'class': "form_check_input"})
+    remember_me = BooleanField('Запомнить меня', default=True, render_kw={'class': "form-check-input"})
     submit = SubmitField('Отправить', render_kw={'class': "btn btn-primary"})
 
 
