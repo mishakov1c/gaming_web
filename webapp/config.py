c8a28583ee500f56537b9fb3d2dcab9eb1249872
Konstantin Mishakov <mishakov1c@gmail.com>
2023-04-06 11:42:34 +0700

    Transition to Postgres

--- webapp/config.py
+++ webapp/config.py
@@ -1,6 +1,7 @@
-import os
-basedir = os.path.dirname(__file__)
+# import os
+# basedir = os.path.dirname(__file__)
 
 MAIN_PAGE = 'http://127.0.0.1:5000/'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://geek_space_admin:159753qwerty@db:5432/geek_space_db'
 SECRET_KEY = 'IHi6hf284efdwiqJKNpo4KFkqdpkm8m1v'
