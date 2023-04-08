70ee9d11c8f45f31848c6ec32d586d311d48362b
Konstantin Mishakov <mishakov1c@gmail.com>
2023-04-08 17:41:12 +0700

    added postgres debugging

---
+++ webapp/config_docker.py
@@ -0,0 +1,6 @@
+import os
+
+MAIN_PAGE = 'http://127.0.0.1:5000/'
+SQLALCHEMY_DATABASE_URI = 'postgresql://geek_space_admin:159753qwerty@db:5432/geek_space_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'IHi6hf284efdwiqJKNpo4KFkqdpkm8m1v'
